a53bb1a2ce8508618e491c8d91a02e24388628ce
Adam <asantillana@apple.com>
2023-03-08 20:49:41 -0600

    fixed naming of user routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const userRoutes = require("./user-routes");
-const thoughtRoutes = require("./thought-routes");
+const userRoutes = require("./userRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
 
-// add prefix of `/users` to routes created in `user-routes.js`
+// add prefix of `/users` to routes created in `userRoutes.js`
 router.use("/users", userRoutes);
-// add prefix of `/thoughts` to routes created in `thought-routes.js`
+// add prefix of `/thoughts` to routes created in `thoughtRoutes.js`
 router.use("/thoughts", thoughtRoutes);
 
 router.use((req, res) => {
