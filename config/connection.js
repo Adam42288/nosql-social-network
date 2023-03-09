f522304c47f92841309f2e3e319def39e2ae2b38
Adam <asantillana@apple.com>
2023-03-08 21:00:53 -0600

    more trouble shooting around getting the connection fixed

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/nosql-social-network",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect("mongodb://localhost:27017/nosql-social-network", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 mongoose.set("debug", true);
 
