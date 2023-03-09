15b36a62f2620b935be30a6f959bb80f7b223dd3
Adam <asantillana@apple.com>
2023-03-08 20:52:53 -0600

    added a new url parser

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/nosql-social-network",
   {
-    useNewUrlParse: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
